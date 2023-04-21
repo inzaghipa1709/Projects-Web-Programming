4e54c2e11e6fc2fc7be4caf1bd33afb26a77784b
Inzaghi Posuma A <108411478+inzaghipa1709@users.noreply.github.com>
2023-04-21 23:36:18 +0700

    Add files via upload

---
+++ Projects-Web-Programming/js/Slick.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
