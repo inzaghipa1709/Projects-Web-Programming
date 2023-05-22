0722a154430d55f990d5fc72f71d9c932f0caaba
Inzaghi Posuma A <108411478+inzaghipa1709@users.noreply.github.com>
2023-05-22 10:32:28 +0700

    Add files via upload

---
+++ Website-Project-New/project/components/footer.php
@@ -0,0 +1,36 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i><span>0821111111</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>1112223333</span></a>
+         <a href="#"><i class="fas fa-envelope"></i><span>RealEstate@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>Jakarta, Indonesia - 10150</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>Home</span></a>
+         <a href="about.php"><span>About</span></a>
+         <a href="contact.php"><span>Contact</span></a>
+         <a href="listings.php"><span>All listings</span></a>
+         <a href="saved.php"><span>Saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="https://www.facebook.com/"><span>Facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="https://twitter.com/"><span>Twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="https://www.linkedin.com/"><span>Linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="https://www.instagram.com/"><span>Instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; Copyright @ <?= date('Y'); ?> by <span>Real Estate Group</span> | All Rights Reserved.</div>
+
+</footer>
+
+<!-- footer section ends -->
